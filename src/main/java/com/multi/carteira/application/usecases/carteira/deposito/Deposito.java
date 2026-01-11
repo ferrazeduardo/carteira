73d9333a408500acbf614c02e126924a0245c7ac
eduardo <eduardo.borges.ferraz@gmail.com>
2026-01-11 00:12:47 -0300

    transactional em Deposito

--- src/main/java/com/multi/carteira/application/usecases/carteira/deposito/Deposito.java
+++ src/main/java/com/multi/carteira/application/usecases/carteira/deposito/Deposito.java
@@ -2,10 +2,12 @@ package com.multi.carteira.application.usecases.carteira.deposito;
 
 import com.multi.carteira.application.gateways.repository.ICarteiraRepository;
 import com.multi.carteira.domain.entities.Carteira;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class Deposito {
 
     @Autowired
