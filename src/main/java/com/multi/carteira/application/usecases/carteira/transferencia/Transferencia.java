ff06d7bfb2aa2c426977b277a05db653176011e1
eduardo <eduardo.borges.ferraz@gmail.com>
2026-01-31 22:34:25 -0300

    data annotation @Service e @Transactional

--- src/main/java/com/multi/carteira/application/usecases/carteira/transferencia/Transferencia.java
+++ src/main/java/com/multi/carteira/application/usecases/carteira/transferencia/Transferencia.java
@@ -2,19 +2,23 @@ package com.multi.carteira.application.usecases.carteira.transferencia;
 
 import com.multi.carteira.application.gateways.repository.ICarteiraRepository;
 import com.multi.carteira.domain.entities.Carteira;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
+@Service
+@Transactional
 public class Transferencia {
 
     @Autowired
-    private ICarteiraRepository _cateiraRespository;
+    private ICarteiraRepository _carteiraRepository;
 
     public void Handler(TransferenciaInput transferenciaInput) {
 
-        Carteira carteiraOrigem = _cateiraRespository.getCarteiraByUpdate(transferenciaInput.contaOrigem());
-        Carteira carteiraDestino = _cateiraRespository.getCarteiraByUpdate(transferenciaInput.contaDestino());
+        Carteira carteiraOrigem = _carteiraRepository.getCarteiraByUpdate(transferenciaInput.contaOrigem());
+        Carteira carteiraDestino = _carteiraRepository.getCarteiraByUpdate(transferenciaInput.contaDestino());
 
         if(transferenciaInput.valor().compareTo(BigDecimal.ZERO) <= 0)
             throw new IllegalArgumentException("Valor deve ser maior que zero");
@@ -27,8 +31,8 @@ public class Transferencia {
 
         carteiraDestino.DepositoValor(transferenciaInput.valor());
 
-        _cateiraRespository.update(carteiraOrigem);
-        _cateiraRespository.update(carteiraDestino);
+        _carteiraRepository.update(carteiraOrigem);
+        _carteiraRepository.update(carteiraDestino);
 
     }
 }
